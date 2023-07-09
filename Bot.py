07a502737dc500a421e6171332b0d4c3147e0183
Yash Pandey <pandeyyash7052@gmail.com>
2023-07-09 06:46:18 +0530

    Updated and replaced open ai api with bard api

--- Bot.py
+++ Bot.py
@@ -1,4 +1,4 @@
-import openai
+from bardapi import Bard
 import pyttsx3
 import speech_recognition as sr
 import os
@@ -6,38 +6,41 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPEN_API_KEY = os.getenv('OPENAI_API_KEY')
+token = os.getenv('COOKIE_KEY')
 
-openai.api_key = OPEN_API_KEY
+bard = Bard(token=token)
 
-#  importing the library
-
-#  intialising the recogniser class for recognising the speech
-
+#  initializing the recognizer class for recognizing speech
 r = sr.Recognizer()
+engine = pyttsx3.init()
+
+engine.say("Hey, I am your assistant for the day. How can I help you?")
+engine.runAndWait()
 
 while True:
     # reading microphone as a source
-
+    ans = ""
     with sr.Microphone() as source:
         # read the audio data from the default microphone
-     try:
-        r.adjust_for_ambient_noise(source)
-        print("Recognizing...")
-        audio_data = r.listen(source)
-        # convert speech to text
-        text = r.recognize_google(audio_data)
-     except:
-      print("sorry not found")
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{
-                                                "role": "user",
-                                                "content": text
-                                            }],
-                                            temperature=0.7)
-    print(response.choices[0].message.content)
-    answer = response.choices[0].message.content
+        try:
+            # Adjust for 1 second to capture ambient noise
+            r.adjust_for_ambient_noise(source, duration=2)
+            print("Recognizing...")
+            audio_data = r.listen(source)
+            # convert speech to text
+            text = r.recognize_google(audio_data, language="en-US")
 
-    engine = pyttsx3.init()
+            ans = bard.get_answer(text)['content']
+
+        except sr.UnknownValueError:
+            print("Speech recognition could not understand audio")
+            engine.say(
+                "Sorry,I can not recognise your voice either be in a quite enviroment or you can come after some time!!!")
+            engine.runAndWait()
+        except sr.RequestError:
+            engine.say(
+                "Could not request results from Google Speech Recognition service")
+            engine.runAndWait()
+    answer = ans
     engine.say(answer)
     engine.runAndWait()
